31ceb86e38bdadfaa40bd76f95c13bda62905d6b
Gleb <glebkabaev200307@gmail.com>
2024-11-14 14:57:57 +0300

    spring security init

--- src/main/java/com/glb/practice/my_practice/MyPracticeApplication.java
+++ src/main/java/com/glb/practice/my_practice/MyPracticeApplication.java
@@ -2,11 +2,22 @@ package com.glb.practice.my_practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @SpringBootApplication
 public class MyPracticeApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MyPracticeApplication.class, args);
-    }
+    public static void main(String[] args) {SpringApplication.run(MyPracticeApplication.class, args);}
+    // //авторизация и генерация пароль user в системе
+    // @Bean
+    // public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+    //     return http.httpBasic().and()
+    //     .authorizeHttpRequests()
+    //     .anyRequest().authenticated().and()
+    //     .build();
+    // }
+    
 }
