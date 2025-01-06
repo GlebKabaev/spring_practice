25cabff385a0fb4fb19a47cc34c0fcca5a6cb423
Gleb <glebkabaev200307@gmail.com>
2025-01-06 15:22:54 +0300

    init

---
+++ src/main/java/com/glb/practice/my_practice/repository/user/UserRepository.java
@@ -0,0 +1,10 @@
+package com.glb.practice.my_practice.repository.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.glb.practice.my_practice.models.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+   
+    User findByUsername(String username); 
+}
