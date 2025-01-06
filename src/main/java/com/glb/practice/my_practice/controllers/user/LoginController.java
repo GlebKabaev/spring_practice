9b5d1275475fade62687c3983eeb5bc670acd1dd
Gleb <glebkabaev200307@gmail.com>
2025-01-06 16:17:03 +0300

    GetMapping has been changed

--- src/main/java/com/glb/practice/my_practice/controllers/user/LoginController.java
+++ src/main/java/com/glb/practice/my_practice/controllers/user/LoginController.java
@@ -2,11 +2,16 @@ package com.glb.practice.my_practice.controllers.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import lombok.AllArgsConstructor;
 
 @Controller
+@RequestMapping({"/login","/login/"})
+@AllArgsConstructor
 public class LoginController {
 
-    @GetMapping("/login")
+    @GetMapping({"/",""})
     public String login() {
         return "login";  
     }
