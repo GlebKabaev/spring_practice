d30024ebd593f9f1ca2b1cb74240bb0cc12e6e1f
Gleb <glebkabaev200307@gmail.com>
2025-01-12 15:23:37 +0300

    rename

--- src/main/java/com/glb/practice/my_practice/srevice/user/UserService.java
+++ src/main/java/com/glb/practice/my_practice/srevice/user/UserService.java
@@ -2,21 +2,26 @@ package com.glb.practice.my_practice.srevice.user;
 
 import com.glb.practice.my_practice.models.User;
 import com.glb.practice.my_practice.repository.user.UserRepository;
+
+import lombok.AllArgsConstructor;
+
 import com.glb.practice.my_practice.auth.CustomUserDetails;
 
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailsService implements UserDetailsService {
+@AllArgsConstructor
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
-
-    public CustomUserDetailsService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -25,5 +30,22 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("User not found with username: " + username);
         }
         return new CustomUserDetails(user);  // Возвращаем CustomUserDetails с данными User
+    }
+    public List<User> getUsers (String field) {
+        return userRepository.findAll(Sort.by(Sort.Order.asc(field)));
+    }
+    public User saveUser(User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+    public User findByIdUser(int id){
+        return userRepository.findById(id).get();
+    }
+    public User updateUser(User user){
+        user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+    public void deleteUser(int id){
+        userRepository.deleteById(id);
     }
 }
