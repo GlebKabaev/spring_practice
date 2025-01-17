39bad713249b6d260cd481392fabe618a3f0fe25
Gleb <glebkabaev200307@gmail.com>
2025-01-17 14:40:08 +0300

    init service for cart

---
+++ src/main/java/com/glb/practice/my_practice/srevice/cart/CartElementService.java
@@ -0,0 +1,15 @@
+package com.glb.practice.my_practice.srevice.cart;
+
+import java.util.List;
+
+import com.glb.practice.my_practice.models.CartElement;
+
+public interface CartElementService {
+
+    public List<CartElement> getCartElements();
+    CartElement saveCartElement(CartElement cart);
+    CartElement findByIDCartElement(int id);
+    CartElement updateCartElement(CartElement cart);
+    void deleteCartElement(int id);  
+} 
+
---
+++ src/main/java/com/glb/practice/my_practice/srevice/cart/CartElementServiceImpl.java
@@ -0,0 +1,48 @@
+package com.glb.practice.my_practice.srevice.cart;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+import com.glb.practice.my_practice.models.CartElement;
+import com.glb.practice.my_practice.repository.cart.CartElementRepository;
+
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+@Primary
+public class CartElementServiceImpl implements CartElementService {
+    CartElementRepository cartRepository;
+
+    @Override
+    public List<CartElement> getCartElements() {
+        
+        return cartRepository.findAll();
+    }
+
+    @Override
+    public CartElement saveCartElement(CartElement cart) {
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public CartElement findByIDCartElement(int id) {
+        return cartRepository.findById(id).get();
+    }
+
+    @Override
+    public CartElement updateCartElement(CartElement cart) {
+        return cartRepository.save(cart);
+    }
+
+    @Override
+    public void deleteCartElement(int id) {
+        cartRepository.deleteById(id);
+    }
+    public List<CartElement> getCartElementsByReaderId(int id) {
+        
+        return cartRepository.findByReaderId(id);
+    } 
+}
