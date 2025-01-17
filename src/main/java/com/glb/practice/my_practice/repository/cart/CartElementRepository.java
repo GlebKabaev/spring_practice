14756009fc44fe168b6fd5385daa436260bbca7a
Gleb <glebkabaev200307@gmail.com>
2025-01-17 14:39:30 +0300

    init repository

---
+++ src/main/java/com/glb/practice/my_practice/repository/cart/CartElementRepository.java
@@ -0,0 +1,11 @@
+package com.glb.practice.my_practice.repository.cart;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.glb.practice.my_practice.models.CartElement;
+
+public interface CartElementRepository extends JpaRepository<CartElement, Integer> {
+    List<CartElement> findByReaderId(Integer readerId);
+}
