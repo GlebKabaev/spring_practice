5382eb2b0c3f606485bffd9dfa66a1db6e841116
Gleb <glebkabaev200307@gmail.com>
2025-01-19 15:33:22 +0300

    home method was deleted, add to cart method was added for user

--- src/main/java/com/glb/practice/my_practice/controllers/user/HomeController.java
+++ src/main/java/com/glb/practice/my_practice/controllers/user/HomeController.java
@@ -3,70 +3,71 @@ package com.glb.practice.my_practice.controllers.user;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.glb.practice.my_practice.models.CartElement;
 import com.glb.practice.my_practice.models.Reader;
-import com.glb.practice.my_practice.models.User;
+
 import com.glb.practice.my_practice.srevice.book.BookService;
-import com.glb.practice.my_practice.srevice.user.UserService;
+import com.glb.practice.my_practice.srevice.cart.CartElementService;
+import com.glb.practice.my_practice.srevice.reader.ReaderService;
 
 import lombok.AllArgsConstructor;
 
-
 @Controller
-@RequestMapping({"/",""})
+@RequestMapping({ "/", "" })
 @AllArgsConstructor
 public class HomeController {
-    BookService bookService;
-    UserService userService;
+    private BookService bookService;
+    private CartElementService cartElementService;
+    private ReaderService readerService;
+    
     @GetMapping("")
     public String defaultPage(@RequestParam String param) {
-        return "redirect:/home/"; 
+        return "redirect:/home/";
     }
-    
-    @GetMapping({"/home","/home/"})
-    public String getMain(Model model){
-      List<String> sortFields=Arrays.asList("Название","Автор","Депозит","Стоимость аренды");
-      UserDetails userDetails =(UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-      User user = userService.findByUserName(userDetails.getUsername());
-        Reader reader = user.getReader();
-        model.addAttribute("sortFields", sortFields);
-        model.addAttribute("books", bookService.getBooks("id"));
-        model.addAttribute("username",reader.getFirstName()+" "+reader.getLastName());
-        return "goods_list";
-    }
-    @GetMapping("/sort")
-    public String sortBooks(@RequestParam("field") String field, Model model) { 
-        UserDetails userDetails =(UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        List<String> sortFields=Arrays.asList("Название","Автор","Депозит","Стоимость аренды");
-        User user = userService.findByUserName(userDetails.getUsername());
-        Reader reader = user.getReader();
+
+    @GetMapping({ "/home", "/home/" })
+    public String sortBooks(@RequestParam(value = "field", required = false, defaultValue = "title") String field,
+            Model model) {
+        Reader reader = readerService.thisReader();
+        List<String> sortFields = Arrays.asList("Название", "Автор", "Депозит", "Стоимость аренды");
         switch (field) {
             case "Название":
-                field= "title";
+                field = "title";
                 break;
             case "Автор":
-                field= "author";
+                field = "author";
                 break;
             case "Депозит":
-                field="depositAmount";
+                field = "depositAmount";
                 break;
             case "Стоимость аренды":
-                field="rentalCost";
+                field = "rentalCost";
             default:
                 break;
         }
+        
         model.addAttribute("sortFields", sortFields);
         model.addAttribute("selectedField", field);
         model.addAttribute("books", bookService.getBooks(field));
-        model.addAttribute("username",reader.getFirstName()+" "+reader.getLastName());
+        model.addAttribute("username", reader.getFirstName() + " " + reader.getLastName());
         return "goods_list";
-        
     }
+
+    @GetMapping({ "/add_to_cart/{id}", "/add_to_cart/{id}/" })
+    public String addToCart(@PathVariable int id) {
+        Reader reader = readerService.thisReader();
+
+        CartElement cartElement = new CartElement(0, reader, bookService.findByIDBook(id));
+        cartElementService.saveCartElement(cartElement);
+
+        return "redirect:/home?field=" ;
+    }
+
 }
