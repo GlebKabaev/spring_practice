bcfc0e8d73d13ee7cd79a14a1fb40a50319efd0a
Gleb <glebkabaev200307@gmail.com>
2025-01-20 15:01:59 +0300

    order endpoint was added

--- src/main/java/com/glb/practice/my_practice/controllers/user/CartController.java
+++ src/main/java/com/glb/practice/my_practice/controllers/user/CartController.java
@@ -1,7 +1,8 @@
 package com.glb.practice.my_practice.controllers.user;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
-
+import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,34 +10,71 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.glb.practice.my_practice.models.Book;
 import com.glb.practice.my_practice.models.CartElement;
 import com.glb.practice.my_practice.models.Reader;
-
+import com.glb.practice.my_practice.models.Rental;
 import com.glb.practice.my_practice.srevice.cart.CartElementServiceImpl;
 import com.glb.practice.my_practice.srevice.reader.ReaderService;
+import com.glb.practice.my_practice.srevice.rental.RentalService;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping({"/cart/","/cart"})
+@RequestMapping({ "/cart/", "/cart" })
 @AllArgsConstructor
 public class CartController {
     CartElementServiceImpl cartElementService;
     ReaderService readerService;
-    @GetMapping({"","/"})
+    RentalService rentalService;
+
+    @GetMapping({ "", "/" })
     public String getMethodName(Model model) {
         Reader reader = readerService.thisReader();
-        List<CartElement> cartElements=cartElementService.getCartElementsByReaderId(reader.getId());
-        model.addAttribute("cart_elements",cartElements);
+        List<CartElement> cartElements = cartElementService.getCartElementsByReaderId(reader.getId());
+        model.addAttribute("cart_elements", cartElements);
         return "reader_cart";
     }
-    @GetMapping({"/delete_element/{id}","/delete_element/{id}/"})
-    public String deleteBook(Model model,@PathVariable int id) {
-        CartElement cartElement=cartElementService.findByIDCartElement(id);
+
+    @GetMapping({ "/delete_element/{id}", "/delete_element/{id}/" })
+    public String deleteBook(Model model, @PathVariable int id) {
+        CartElement cartElement = cartElementService.findByIDCartElement(id);
         Reader reader = readerService.thisReader();
-        if (reader.getId().equals(cartElement.getReader().getId())){
-        cartElementService.deleteCartElement(id);
+        if (reader.getId().equals(cartElement.getReader().getId())) {
+            cartElementService.deleteCartElement(id);
         }
         return "redirect:/cart";
     }
+//TODO нельзя добавлять книги которых нет в наличии 
+//TODO не должно возвращаться при пустом заказе
+//TODO добавить возможность выбрать адресс для заказа
+    @GetMapping("/order")
+    public String order(@RequestParam("expectedReturnDate") String expectedReturnDate) {
+        Reader reader = readerService.thisReader();
+        List<CartElement> cartElements = cartElementService.getCartElementsByReaderId(reader.getId());
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        Date expectedReturn = null;
+        try {
+            expectedReturn = sdf.parse(expectedReturnDate);
+        } catch (Exception e) {
+            e.printStackTrace();
+
+            return "redirect:/cart";
+        }
+        for (CartElement cartElement : cartElements) {
+            Book book = cartElement.getBook();
+            Rental rental = new Rental();
+            rental.setBook(book);
+            rental.setReader(reader);
+            rental.setExpectedReturnDate(expectedReturn);
+            rental.setIssueDate(expectedReturn);
+            rentalService.saveRental(rental);
+            cartElementService.deleteCartElement(cartElement.getId());
+        }
+        return "order";
+    }
+
+    
+
 }
