745922e4af37f588cadccaa5d93a63609b0c17b4
Gleb <glebkabaev200307@gmail.com>
2025-01-22 14:10:01 +0300

    POST http request with sort field was added

--- src/main/java/com/glb/practice/my_practice/controllers/user/HomeController.java
+++ src/main/java/com/glb/practice/my_practice/controllers/user/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -31,12 +32,16 @@ public class HomeController {
     public String defaultPage(@RequestParam String param) {
         return "redirect:/home/";
     }
-
+    //TODO добавить ручку "заказы"
     @GetMapping({ "/home", "/home/" })
     public String sortBooks(@RequestParam(value = "field", required = false, defaultValue = "title") String field,
             Model model) {
         Reader reader = readerService.thisReader();
         List<String> sortFields = Arrays.asList("Название", "Автор", "Депозит", "Стоимость аренды");
+        if(sortFields.contains(field)){
+        sortFields.set(sortFields.indexOf(field),sortFields.get(0));
+        sortFields.set(0,field);
+        }
         switch (field) {
             case "Название":
                 field = "title";
@@ -60,14 +65,15 @@ public class HomeController {
         return "goods_list";
     }
 
-    @GetMapping({ "/add_to_cart/{id}", "/add_to_cart/{id}/" })
-    public String addToCart(@PathVariable int id) {
+    @PostMapping({ "/add_to_cart/{id}", "/add_to_cart/{id}/" })
+    public String addToCart(@PathVariable int id,@RequestParam(value = "field", required = false, defaultValue = "title") String field) {
         Reader reader = readerService.thisReader();
 
         CartElement cartElement = new CartElement(0, reader, bookService.findByIDBook(id));
         cartElementService.saveCartElement(cartElement);
-
-        return "redirect:/home?field=" ;
+        
+        
+        return "redirect:/home?field="+field ;
     }
 
 }
