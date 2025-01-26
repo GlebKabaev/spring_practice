79830d47371eb5c2ebff337175016b36c8b0e6ad
Gleb <glebkabaev200307@gmail.com>
2025-01-26 16:28:39 +0300

    findByReader method

--- src/main/java/com/glb/practice/my_practice/repository/rental/RentalRepository.java
+++ src/main/java/com/glb/practice/my_practice/repository/rental/RentalRepository.java
@@ -1,10 +1,13 @@
 package com.glb.practice.my_practice.repository.rental;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
+import com.glb.practice.my_practice.models.Reader;
 import com.glb.practice.my_practice.models.Rental;
 
 public interface RentalRepository extends JpaRepository<Rental,Integer> {
+    List<Rental> findByReader(Reader reader);
     
 }
