129989d93bf62ac8c5d1bd65aa33a5ecb319a40d
Gleb <glebkabaev200307@gmail.com>
2025-01-26 16:30:16 +0300

    getCartElements method was changed

--- src/main/java/com/glb/practice/my_practice/srevice/cart/CartElementService.java
+++ src/main/java/com/glb/practice/my_practice/srevice/cart/CartElementService.java
@@ -6,7 +6,7 @@ import com.glb.practice.my_practice.models.CartElement;
 
 public interface CartElementService {
 
-    public List<CartElement> getCartElements();
+    public List<CartElement> getCartElements(String field);
     CartElement saveCartElement(CartElement cart);
     CartElement findByIDCartElement(int id);
     CartElement updateCartElement(CartElement cart);
