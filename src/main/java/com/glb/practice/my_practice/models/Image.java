b94213630eac06610c3b6fd2942982f2dc954bc8
Gleb <glebkabaev200307@gmail.com>
2025-01-27 17:38:37 +0300

    rename contentType field

--- src/main/java/com/glb/practice/my_practice/models/Image.java
+++ src/main/java/com/glb/practice/my_practice/models/Image.java
@@ -8,21 +8,19 @@ import lombok.*;
 @Entity
 @Table(name = "images")
 @AllArgsConstructor
+@NoArgsConstructor
 public class Image {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
+    
     @Column(name = "name")
     private String name;
-
-    @Column(name = "type")
-    private String type;
-
+    
+    @Column(name = "content_type")
+    private String contentType;
+    
     @Lob
     @Column(name = "image_data")
     private byte[] imageData;
-    public Image(){
-        
-    }
 }
