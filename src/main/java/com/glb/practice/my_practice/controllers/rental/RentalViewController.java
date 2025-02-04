47c56a08d83a029ee1c9b0469a93fb73550c7896
Gleb <glebkabaev200307@gmail.com>
2025-02-04 12:57:38 +0300

    code format

--- src/main/java/com/glb/practice/my_practice/controllers/rental/RentalViewController.java
+++ src/main/java/com/glb/practice/my_practice/controllers/rental/RentalViewController.java
@@ -1,4 +1,5 @@
 package com.glb.practice.my_practice.controllers.rental;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ import com.glb.practice.my_practice.srevice.reader.ReaderService;
 import com.glb.practice.my_practice.srevice.rental.RentalService;
 
 import lombok.AllArgsConstructor;
+
 @Controller
 @RequestMapping("/admin/rentals")
 @AllArgsConstructor
@@ -20,55 +22,63 @@ public class RentalViewController {
     private final RentalService rentalService;
     private final ReaderService readerService;
     private final BookService bookService;
-    @GetMapping({"/",""})
+
+    @GetMapping({ "/", "" })
     public String showRentals(Model model) {
-        
+
         model.addAttribute("rentals", rentalService.getRentals());
-    return "rental_list";
+        return "rental_list";
     }
-    @GetMapping({"/{id}","/{id}/"})
+
+    @GetMapping({ "/{id}", "/{id}/" })
     public String showRentalData(Model model, @PathVariable int id) {
         model.addAttribute("rental", rentalService.findByIDRental(id));
         return "rental";
     }
-    @GetMapping({"/new","/new/"})
+
+    @GetMapping({ "/new", "/new/" })
     public String showCreateRentalForm(Model model) {
         model.addAttribute("readers", readerService.getReaders("id"));
         model.addAttribute("books", bookService.getNotZeroBooks());
-        model.addAttribute("rental", new Rental()); 
-    return "rental_add-edit"; 
+        model.addAttribute("rental", new Rental());
+        return "rental_add-edit";
     }
-    @PostMapping({"/save_rental", "/save_rental/"})
+
+    @PostMapping({ "/save_rental", "/save_rental/" })
     public String saveRental(@ModelAttribute("rental") Rental rental, Model model) {
         try {
-        rentalService.saveRental(rental);
+            rentalService.saveRental(rental);
         } catch (Exception e) {
             e.printStackTrace();
             model.addAttribute("readers", readerService.getReaders("id"));
             model.addAttribute("books", bookService.getNotZeroBooks());
-            model.addAttribute("rental", new Rental()); 
+            model.addAttribute("rental", new Rental());
             model.addAttribute("error", e.getMessage());
             return "rental_add-edit";
         }
-        return "redirect:/admin/rentals"; 
+
+        return "redirect:/admin/rentals";
     }
-    @GetMapping({"/delete/{id}","/delete/{id}/"})
-    public String deleteRental(Model model,@PathVariable int id) {
+
+    @GetMapping({ "/delete/{id}", "/delete/{id}/" })
+    public String deleteRental(Model model, @PathVariable int id) {
         rentalService.deleteRental(id);
         return "redirect:/admin/rentals";
     }
-    @GetMapping({"/edit/{id}","/edit/{id}/"})
+
+    @GetMapping({ "/edit/{id}", "/edit/{id}/" })
     public String editRental(@PathVariable int id, Model model) {
         model.addAttribute("readers", readerService.getReaders("id"));
         model.addAttribute("books", bookService.getNotZeroBooks());
         model.addAttribute("rental", rentalService.findByIDRental(id));
         return "rental_add-edit";
     }
-    @PostMapping({"/update_rental","/update_rental/"})
-    public String updateRental(@ModelAttribute("rental") Rental rental,Model model) {
+
+    @PostMapping({ "/update_rental", "/update_rental/" })
+    public String updateRental(@ModelAttribute("rental") Rental rental, Model model) {
         try {
             rentalService.updateRental(rental);
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             model.addAttribute("readers", readerService.getReaders("id"));
             model.addAttribute("books", bookService.getNotZeroBooks());
