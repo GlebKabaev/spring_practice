c430d4e913b3a22ba2c139a6ff0b2237f16233c7
Gleb <glebkabaev200307@gmail.com>
2025-02-11 14:16:40 +0300

    Modified views for the UserReader controller, fixed the entity saving method.

--- src/main/java/com/glb/practice/my_practice/controllers/userReader/UserReaderViewController.java
+++ src/main/java/com/glb/practice/my_practice/controllers/userReader/UserReaderViewController.java
@@ -8,34 +8,32 @@ import com.glb.practice.my_practice.srevice.user.UserService;
 import com.glb.practice.my_practice.srevice.userReader.UserReaderService;
 
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 //TODO: переделать все mapping на верные
 //TODO:изучить restfullapi еще больше
 //TODO:создать сущность user reader
 @Controller
-@RequestMapping("/admin/users-readersV2")
+@RequestMapping("/admin/users-readers")
 @AllArgsConstructor
 public class UserReaderViewController {
     private final UserReaderService userReaderService;
     private final CartElementService cartElementService;
     private final BookService bookService;
-    private final UserService us;
+    private final UserService userService;
+    private final ReaderService readerService;
 
     /** Показать список пользователей и читателей */
     // сортировку добавить
     @GetMapping({ "/", "" })
     public String showUsersAndReaders(Model model) {
-        //List<String> sortFields = Arrays.asList("id", "username", "Фамилия", "Имя", "Отчество");
-       // model.addAttribute("sortFields", sortFields);
+        // List<String> sortFields = Arrays.asList("id", "username", "Фамилия", "Имя",
+        // "Отчество");
+        // model.addAttribute("sortFields", sortFields);
         model.addAttribute("UsersReaders", userReaderService.findAll());
         return "user_reader_list";
     }
@@ -62,67 +60,81 @@ public class UserReaderViewController {
 
     /** Просмотр информации о пользователе и его читателе */
     @GetMapping("/{userReaderId}")
-    public String showUserAndReader(@PathVariable int userId, Model model) {
-        model.addAttribute("userReader", userReaderService.findById(userId));
+    public String showUserAndReader(@PathVariable int userReaderId, Model model) {
+        model.addAttribute("userReader", userReaderService.findById(userReaderId));
         return "user_reader";
     }
 
     /** Форма создания нового пользователя с читателем */
     @GetMapping("/new")
     public String showCreateUserReaderForm(Model model) {
-        model.addAttribute("userReader", new UserReader());
+        model.addAttribute("reader", new Reader());
+        model.addAttribute("user", new User());
         return "user_reader_add-edit";
     }
 
     /** Сохранение нового пользователя с читателем */
     @PostMapping("/save")
-    public String saveUserAndReader(@ModelAttribute("userReader") UserReader userReader,
+    public String saveUserAndReader(@ModelAttribute("user") User user, @ModelAttribute("reader") Reader reader,
             Model model) {
         try {
-            userReaderService.save(userReader);
+            userService.saveUser(user);
+            readerService.save(reader);
+            userReaderService.save(new UserReader(0, user, reader));
         } catch (Exception e) {
-            model.addAttribute("user", userReader);
+            model.addAttribute("reader", reader);
+            model.addAttribute("user", user);
             model.addAttribute("error", e.getMessage());
+            if (user.getId() != null) {
+                userService.deleteUser(user.getId());
+            }
+            if (reader.getId() != null) {
+                readerService.deleteReader(reader.getId());
+            }
             return "user_reader_add-edit";
         }
-        return "redirect:/admin/users-readersV2";
+        return "redirect:/admin/users-readers";
     }
 
     /** Удаление пользователя и его читателя */
-    @GetMapping("/delete/{userReaderID}")
-    public String deleteUserAndReader(@PathVariable int userId) {
-        us.deleteUser(userId);
-        return "redirect:/admin/users-readersV2";
+    @GetMapping("/{userReaderId}/delete")
+    public String deleteUserAndReader(@PathVariable int userReaderId) {
+        userService.deleteUser(userReaderId);
+        return "redirect:/admin/users-readers";
     }
 
     /** Форма редактирования пользователя и его читателя */
-    @GetMapping("/edit/{userReaderID}")
-    public String editUserAndReader(@PathVariable int userId, Model model) {
-        UserReader userReader = userReaderService.findById(userId);
-        model.addAttribute("userReader", userReader);
+    @GetMapping("/{userReaderId}/edit")
+    public String editUserAndReader(@PathVariable int userReaderId, Model model) {
+        UserReader userReader = userReaderService.findById(userReaderId);
+
+        model.addAttribute("reader", userReader.getReader());
+        model.addAttribute("user", userReader.getUser());
         return "user_reader_add-edit";
     }
 
     /** Обновление данных пользователя и его читателя */
     // TODO проблема изменения reader так как он принимает id значение своего user
     @PostMapping("/update")
-    public String updateUserAndReader(@ModelAttribute("userReader") UserReader userReader,
+    public String updateUserAndReader(@ModelAttribute("user") User user, @ModelAttribute("reader") Reader reader,
             Model model) {
         try {
-           userReaderService.update(userReader);
+            userService.updateUser(user);
+            readerService.updateReader(reader);
         } catch (Exception e) {
-            model.addAttribute("userReader", userReader);
+            model.addAttribute("reader", reader);
+            model.addAttribute("user", user);
             model.addAttribute("error", e.getMessage());
             return "user_reader_add-edit";
         }
-        return "redirect:/admin/users-readersV2";
+        return "redirect:/admin/users-readers";
     }
 
     /** Просмотр корзины читателя */
     @GetMapping("/{userReaderID}/cart")
     public String showReaderCart(@PathVariable int userReaderId, Model model) {
-        UserReader userReader=userReaderService.findById(userReaderId);
-        Reader reader=userReader.getReader();
+        UserReader userReader = userReaderService.findById(userReaderId);
+        Reader reader = userReader.getReader();
         List<CartElement> cartElements = cartElementService.findByReaderId(reader.getId());
 
         model.addAttribute("reader", reader);
@@ -133,8 +145,8 @@ public class UserReaderViewController {
     /* Форма добавления книги в корзину */
     @GetMapping("/{userReaderID}/cart/new")
     public String newBookToReaderCart(@PathVariable int userReaderId, Model model) {
-        UserReader userReader=userReaderService.findById(userReaderId);
-        Reader reader=userReader.getReader();
+        UserReader userReader = userReaderService.findById(userReaderId);
+        Reader reader = userReader.getReader();
 
         List<Book> books = bookService.findByQuantityNotZeroAndDeletedFalse();
         model.addAttribute("reader", reader);
@@ -145,13 +157,12 @@ public class UserReaderViewController {
     /** Добавление книги в корзину читателя */
     @PostMapping("/{userReaderID}/cart/new")
     public String addBookToReaderCart(@PathVariable int userReaderId, @RequestParam("bookId") int bookId) {
-        UserReader userReader=userReaderService.findById(userReaderId);
-       
+        UserReader userReader = userReaderService.findById(userReaderId);
 
         Reader reader = userReader.getReader();
         Book book = bookService.findById(bookId);
         cartElementService.save(new CartElement(0, reader, book));
 
-        return "redirect:/admin/users-readersV2/%d/cart".formatted(userReaderId);
+        return "redirect:/admin/users-readers/%d/cart".formatted(userReaderId);
     }
 }
