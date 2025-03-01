359947c9893b58c3224b02549d9ca0fdd820b00d
Gleb <glebkabaev200307@gmail.com>
2025-03-01 17:17:38 +0300

    interface was deleted

--- src/main/java/com/glb/practice/my_practice/service/book/BookService.java
+++ src/main/java/com/glb/practice/my_practice/service/book/BookService.java
@@ -1,18 +1,68 @@
 package com.glb.practice.my_practice.service.book;
 
 import java.util.List;
-
-
-
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import com.glb.practice.my_practice.models.Book;
+import com.glb.practice.my_practice.models.CartElement;
+import com.glb.practice.my_practice.repository.book.BookRepository;
+import com.glb.practice.my_practice.service.cart.CartElementService;
 
+import lombok.AllArgsConstructor;
 
-public interface BookService {
-    List<Book> findAll(String field);
-    List<Book> findByQuantityNotZeroAndDeletedFalse();
-    List<Book> getNotZeroSortedBooks(String field);
-    Book save(Book book);
-    Book findById(int id);
-    Book update(Book book);
-    void deleteById(int id);
+@Service
+@AllArgsConstructor
+@Primary
+public class BookService {
+    private final CartElementService cartElementService;
+    private final BookRepository bookRepository;
+
+    @Transactional(readOnly = true)
+    public List<Book> findAll(String field) {
+        return bookRepository.findAll(Sort.by(Sort.Order.asc(field)));
+    }
+
+    @Transactional(readOnly = true)
+    public List<Book> findByQuantityNotZeroAndDeletedFalse() {
+        return bookRepository.findByQuantityNotAndDeletedFalse(0, Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    @Transactional
+    public Book save(Book book) {
+        if (book.getQuantity() >= 0 && book.getRentalCost() >= 0 && book.getDepositAmount() >= 0) {
+            return bookRepository.save(book);
+        } else {
+            throw new IllegalArgumentException("поле не может быть отрицательным");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public Book findById(int id) {
+        return bookRepository.findById(id).orElseThrow(() -> new RuntimeException("Book not found"));
+    }
+
+    @Transactional
+    public Book update(Book book) {
+        if (book.getQuantity() >= 0 && book.getRentalCost() >= 0 && book.getDepositAmount() >= 0) {
+            return bookRepository.save(book);
+        } else {
+            throw new IllegalArgumentException("поле не может быть отрицательным");
+        }
+    }
+
+    @Transactional
+    public void deleteById(int id) {
+        bookRepository.findById(id).get().setDeleted(true);
+        List<CartElement> cart = cartElementService.findByBookId(id);
+        for (CartElement cartElement : cart) {
+            cartElementService.deleteById(cartElement.getId());
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public List<Book> getNotZeroSortedBooks(String field) {
+        return bookRepository.findByQuantityNotAndDeletedFalse(0, Sort.by(Sort.Direction.DESC, field));
+    }
 }
