bd4442a111ad1ad70fa08be067ce9d53d3158588
Gleb <glebkabaev200307@gmail.com>
2025-03-01 15:28:06 +0300

    interface was deleted.Method toggleRentalStatus() was added

--- src/main/java/com/glb/practice/my_practice/service/rental/RentalService.java
+++ src/main/java/com/glb/practice/my_practice/service/rental/RentalService.java
@@ -1,17 +1,88 @@
 package com.glb.practice.my_practice.service.rental;
 
+import java.util.Date;
 import java.util.List;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.glb.practice.my_practice.exception.RentalNotFoundException;
+import com.glb.practice.my_practice.models.Book;
 import com.glb.practice.my_practice.models.Reader;
 import com.glb.practice.my_practice.models.Rental;
+import com.glb.practice.my_practice.repository.rental.RentalRepository;
+import com.glb.practice.my_practice.service.book.BookService;
 
-public interface RentalService {
-    public List<Rental> findAll();
-    Rental save(Rental rental);
-    Rental findById(int id);
-    Rental update(Rental rental);
-    void deleteById(int id);
-    public List<Rental> findByReader(Reader reader);
-    public List<Rental> findAllExpiredRentals();
-    
-} 
+import lombok.AllArgsConstructor;
+@Service
+@AllArgsConstructor
+@Primary
+public class RentalService  {
+    RentalRepository rentalRepository;
+    BookService bookService;
+
+    @Transactional(readOnly = true)
+    public List<Rental> findAll() {
+        return rentalRepository.findAll(Sort.by(Sort.Direction.DESC, "id"));
+    }
+
+    @Transactional
+    public Rental save(Rental rental) {
+        Book book = bookService.findById(rental.getBook().getId());
+        if (rental.getBook().isDeleted()) {
+            throw new IllegalArgumentException("данная книга удалена");
+        }
+        if (rental.getIssueDate().after(rental.getExpectedReturnDate())) {
+            throw new IllegalArgumentException("дата выдачи не может быть позже даты возврата");
+        }
+        if (book.getQuantity() > 0) {
+            book.setQuantity(book.getQuantity() - 1); 
+            bookService.save(book); 
+            rental.setReturned(false);
+            return rentalRepository.save(rental); 
+        } else {
+            throw new IllegalArgumentException("книги нет в наличии");
+        }
+
+    }
+
+    @Transactional(readOnly = true)
+    public Rental findById(int id) {
+        return rentalRepository.findById(id).get();
+    }
+
+    @Transactional
+    public Rental update(Rental rental) {
+        if (rental.getIssueDate().after(rental.getExpectedReturnDate())) {
+            throw new IllegalArgumentException("дата выдачи не может быть позже даты возврата");
+        }
+        return rentalRepository.save(rental);
+    }
+
+    @Transactional
+    public void deleteById(int id) {
+        rentalRepository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Rental> findByReader(Reader reader) {
+        return rentalRepository.findByReader(reader);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Rental> findAllExpiredRentals() {
+        Date date = new Date();
+        List<Rental>expiredRentals =rentalRepository.findByExpectedReturnDateBeforeAndReturnedFalse(date);
+        return expiredRentals;
+    }
+    public Rental toggleRentalStatus(int id) {
+        Rental rental = findById(id);
+        if (rental == null) {
+            throw new RentalNotFoundException("Rental with id " + id + " not found");
+        }
+        rental.setReturned(!rental.getReturned());
+        return update(rental);
+    }
+}
