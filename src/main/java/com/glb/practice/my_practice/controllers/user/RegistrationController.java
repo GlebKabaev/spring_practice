4bb149179c617cc90e5dd186a9306dc072d3a71b
Gleb <glebkabaev200307@gmail.com>
2025-03-09 13:14:08 +0300

    confirm password was added

--- src/main/java/com/glb/practice/my_practice/controllers/user/RegistrationController.java
+++ src/main/java/com/glb/practice/my_practice/controllers/user/RegistrationController.java
@@ -1,12 +1,12 @@
 package com.glb.practice.my_practice.controllers.user;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.glb.practice.my_practice.models.Reader;
 import com.glb.practice.my_practice.models.User;
@@ -16,55 +16,63 @@ import com.glb.practice.my_practice.service.user.UserService;
 import lombok.AllArgsConstructor;
 
 @Controller
-@RequestMapping({"/register","/register/"})
+@RequestMapping({ "/register", "/register/" })
 @AllArgsConstructor
 public class RegistrationController {
 
     private final UserService userService;
     private final ReaderService readerService;
-    @GetMapping({"/",""})
+
+    @GetMapping({ "/", "" })
     public String register(Model model) {
         model.addAttribute("reader", new Reader());
         model.addAttribute("user", new User());
-        return "register";  // Страница для регистрации
+        return "register"; // Страница для регистрации
     }
 
-    @PostMapping({"/new","/new/"})
-    public String registerUser(@ModelAttribute("user") User user,@ModelAttribute("reader") Reader reader,Model model) {
+    @PostMapping({ "/new", "/new/" })
+    public String registerUser(@ModelAttribute("user") User user, @ModelAttribute("reader") Reader reader, Model model,
+            @RequestParam("confirmPassword") String confirmPassword) {
         boolean isUserCreated;
-        //TODO подтвердите пароль
+        // TODO подтвердите пароль
         user.setRole("ROLE_USER");
-        try{
-        userService.saveUser(user);
-        isUserCreated=true;
-        }catch(Exception e){
-            isUserCreated=false;
+        try {
+            if (user.getPassword().equals(confirmPassword)) {
+                userService.saveUser(user);
+                isUserCreated = true;
+            } else {
+                throw new Exception("поле подтверждения пароля не совпадает");
+            }
+        } catch (Exception e) {
+            isUserCreated = false;
             model.addAttribute("user", user);
             if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-                model.addAttribute("error", "Пользователь с таким логином уже существует. Пожалуйста, укажите другой логин.");
+                model.addAttribute("error",
+                        "Пользователь с таким логином уже существует. Пожалуйста, укажите другой логин.");
             } else {
-                model.addAttribute("error",e.getMessage());
+                model.addAttribute("error", e.getMessage());
             }
             return "register";
         }
-        if (isUserCreated){
-        try{
-            readerService.save(reader); 
-            userService.addReaderForUser(user, reader);
-            }catch(Exception e){
+        if (isUserCreated) {
+            try {
+                readerService.save(reader);
+                userService.addReaderForUser(user, reader);
+            } catch (Exception e) {
                 e.printStackTrace();
-                
+
                 model.addAttribute("reader", reader);
                 if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
-                    model.addAttribute("error", "Пользователь с таким номером телефона уже существует. Пожалуйста, укажите другой номер.");
+                    model.addAttribute("error",
+                            "Пользователь с таким номером телефона уже существует. Пожалуйста, укажите другой номер.");
                 } else {
-                    model.addAttribute("error",e.getMessage());
+                    model.addAttribute("error", e.getMessage());
                 }
                 userService.deleteUser(user.getId());
                 return "register";
             }
-        return "redirect:/login";  // Перенаправление на страницу логина
-        }else return "register";
+            return "redirect:/login"; // Перенаправление на страницу логина
+        } else
+            return "register";
     }
 }
-
