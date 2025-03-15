57aecba953f16b4f52f5a45cba2e049ab455931e
Gleb <glebkabaev200307@gmail.com>
2025-03-15 15:57:37 +0300

    deleted usless interface

--- src/main/java/com/glb/practice/my_practice/service/userReader/UserReaderService.java
+++ src/main/java/com/glb/practice/my_practice/service/userReader/UserReaderService.java
@@ -1,23 +1,46 @@
 package com.glb.practice.my_practice.service.userReader;
 
+import lombok.AllArgsConstructor;
 import java.util.List;
-
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
 import com.glb.practice.my_practice.models.Reader;
 import com.glb.practice.my_practice.models.User;
 import com.glb.practice.my_practice.models.UserReader;
+import com.glb.practice.my_practice.repository.userReader.UserReaderRepository;
 
-public interface UserReaderService {
-    public List<UserReader> findAll();
+@Service
+@AllArgsConstructor
+@Primary
+public class UserReaderService  {
+    UserReaderRepository userReaderRepository;
 
-    UserReader save(UserReader userReader);
+    public List<UserReader> findAll() {
+        return userReaderRepository.findAll();
+    }
 
-    UserReader findById(int id);
+    public UserReader save(UserReader userReader) {
+        return userReaderRepository.save(userReader);
+    }
 
-    UserReader update(UserReader userReader);
+    public UserReader findById(int id) {
+        return userReaderRepository.findById(id).get();
+    }
 
-    void deleteById(int id);
+    public UserReader update(UserReader userReader) {
+        return userReaderRepository.save(userReader);
+    }
 
-    public UserReader findByReader(Reader reader);
+    public void deleteById(int id) {
+        userReaderRepository.deleteById(id);
+    }
 
-    public UserReader findByUser(User user);
+    public UserReader findByReader(Reader reader) {
+        return userReaderRepository.findByReader(reader);
+    }
+
+    public UserReader findByUser(User user) {
+        return userReaderRepository.findByUser(user);
+    }
+
 }
